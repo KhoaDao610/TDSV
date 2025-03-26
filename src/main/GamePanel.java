681cbfd2f5a5048e1aaf34d64ad2e9cd954673b1
Hoang Cong Khoi <24022371@vnu.edu.vn>
2025-03-27 00:16:18 +0700

    Fist steo

---
+++ src/main/GamePanel.java
@@ -0,0 +1,93 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    final int originalTileSize = 16;//16x16 tile
+    final int scale=3;
+
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = maxScreenCol * tileSize; //768 pixels
+    final int screenHeight = maxScreenRow * tileSize; //576 pixels
+
+    //Player's information:
+    int playerSpeed = 4;
+    int playerX = screenWidth / 2;
+    int playerY = screenHeight / 2;
+    int FPS = 60;
+
+
+    KeyHandler keyHandler = new KeyHandler();
+    Thread gameThread;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(Color.WHITE);
+
+        g2d.fillRect(playerX,playerY, tileSize, tileSize);
+
+        g2d.dispose();
+    }
+
+    public void update() {
+        if (keyHandler.up) {
+            playerY -= playerSpeed;
+        }
+        else if (keyHandler.down){
+            playerY += playerSpeed;
+        }
+        else if (keyHandler.left){
+            playerX -= playerSpeed;
+        }
+        else if (keyHandler.right){
+            playerX += playerSpeed;
+        }
+
+    }
+
+
+    @Override
+    public void run() {
+
+        double drawInterval =1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+
+        while(gameThread!=null) {
+
+            long now = System.nanoTime();
+            delta += (now - lastTime) / drawInterval;
+            lastTime = now;
+
+            while (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+
+        }
+    }
+
+}
+
---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setTitle("Bombaclat");
+
+        main.GamePanel gamePanel = new main.GamePanel();
+        frame.add(gamePanel);
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
